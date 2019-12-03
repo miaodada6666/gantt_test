9ab1bc1b03bfd2d9ad7c9326d2fea231773d0945
Miao Kenny <kenny.miao@sap.com>
2019-12-03 06:32:29 +0000

    fix some codes

--- local-dev-test-master/app_EE/webapp/Component.js
+++ local-dev-test-master/app_EE/webapp/Component.js
@@ -1,7 +1,28 @@
-sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"],
+function (AppComponent) {
 	return AppComponent.extend("app_EE.Component", {
-		metadata: {
-			"manifest": "json"
+		
+			metadata : {
+			rootView : "com.sap.gantt.gantt.Component",
+			dependencies : {
+				libs : [
+					"sap.gantt",
+					"sap.ui.table",
+					"sap.m"
+				]
+			},
+			config : {
+				sample : {
+					files : [
+						"gantt.view.xml",
+						"gantt.controller.js",
+						"data.json"
+					]
+				}
+			}
 		}
+		// metadata: {
+		// 	"manifest": "json"
+		// }
 	});
 });
