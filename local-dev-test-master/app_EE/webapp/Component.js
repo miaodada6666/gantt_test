a89d99eb8d015933187f08f5063839f141cf9982
miaodada6666 <1005780452@qq.com>
2019-12-05 14:24:58 +0800

    add ui5 codes

--- local-dev-test-master/app_EE/webapp/Component.js
+++ local-dev-test-master/app_EE/webapp/Component.js
@@ -1,28 +1,8 @@
 sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"],
-function (AppComponent) {
-	return AppComponent.extend("app_EE.Component", {
-		
-			metadata : {
-			rootView : "com.sap.gantt.gantt.Component",
-			dependencies : {
-				libs : [
-					"sap.gantt",
-					"sap.ui.table",
-					"sap.m"
-				]
-			},
-			config : {
-				sample : {
-					files : [
-						"gantt.view.xml",
-						"gantt.controller.js",
-						"data.json"
-					]
-				}
-			}
-		}
-		// metadata: {
-		// 	"manifest": "json"
-		// }
-	});
-});
+    function(AppComponent) {
+        return AppComponent.extend("app_EE.Component", {
+            metadata: {
+                "manifest": "json"
+            }
+        });
+    });
